402af02c651859a448abcf0b460b7ad7f69d5692
Robin <rdekogel@roc-dev.com>
2017-09-11 14:21:29 +0200

    Opdracht 6.3

---
+++ src/H06/Positieve.java
@@ -0,0 +1,39 @@
+package H06;/*
+
+@(#)Show.java 1.0 03/02/26
+
+*
+
+Voorbeeld 2.1
+
+*
+
+*/
+
+//importeer de benodigde klassen uit de Java bibliotheek
+
+import sun.text.resources.ro.CollationData_ro;
+
+import java.applet.Applet;
+import java.awt.*;
+
+
+public class Positieve extends Applet {
+
+    int  a, b , uitkomst;
+
+
+    public void init() {
+        a = 938920697;
+        b = 574752652;
+        uitkomst = ( a * b );
+
+
+    }
+
+    public void paint(Graphics g) {
+        g.drawString("De uitkomst is:  " + uitkomst,20,20);
+
+    }
+
+}
