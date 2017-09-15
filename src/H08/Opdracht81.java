9e21abb5a5bd0e63e9f32e744daa6e6eb991fa2f
Robin <rdekogel@roc-dev.com>
2017-09-15 13:17:27 +0200

    Opdracht 8_1

--- src/H08/Opdracht81.java
+++ src/H08/Opdracht81.java
@@ -1,43 +1,59 @@
-package H08;
-
-import java.applet.Applet;
 import java.awt.*;
+import java.applet.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 
-
-
-
 public class Opdracht81 extends Applet {
-    TextField txtinput;
-    Button oke;
-    Button opnieuw;
-
+    TextField tekstvlak;
+    Button knop;
+    Button knop2;
+    Label label;
+    String s;
+    String d;
 
     public void init() {
-        txtinput = new TextField("",50);
-        label = new label ("Laat hier uw bericht achter");
-        oke = new Button("oke");
-        opnieuw = new Button("Reset");
-        add ( oke );
-        add( opnieuw );
+        tekstvlak = new TextField(" ",20);
+        label = new Label("Type hier uw bericht " + "");
+        tekstvlak.addActionListener( new tekstvlakListener() );
+        knop = new Button("Reset");
+        knop2 = new Button("oke");
+        KnopListener kl = new KnopListener();
+        Knop2Listener rl = new Knop2Listener();
+        knop.addActionListener((kl));
+        knop2.addActionListener(rl);
+        add(knop);
+        add(knop2);
+        add (label);
+        add ( tekstvlak );
+        s = "";
+        d = "";
+    }
 
+    public void paint (Graphics g) {
+        g.drawString(s, 50,50);
 
     }
 
-    public void paint(Graphics g) {
-    g.drawString("Vul hier uw gegvens in ",70,70);
-
-    }
-
-
-    class opnieuwListener implements ActionListener {
-        public void actionPerformed ( ActionEvent e) {
-            txtinput.setText("");
+    class  tekstvlakListener implements ActionListener {
+        public void actionPerformed (ActionEvent e) {
+            s = tekstvlak.getText();
             repaint();
         }
+    }
 
+    private class KnopListener implements ActionListener {
+        @Override
+        public void actionPerformed(ActionEvent e) {
+            s = d ;
+            repaint();
+        }
+    }
+
+
+    private class Knop2Listener implements  ActionListener{
+        public  void  actionPerformed(ActionEvent e) {
+            s = tekstvlak.getText();
+            repaint();
+        }
     }
 }
-
-
