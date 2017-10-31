29de06e2b3f65a14e6746eda42b46982e69abaf3
Robin <rdekogel@roc-dev.com>
2017-10-31 09:52:43 +0100

    Opdracht11-6

--- src/H11/Opdracht11_6.java
+++ src/H11/Opdracht11_6.java
@@ -2,20 +2,21 @@ package H11;
 import java.awt.*;
 import java.applet.*;
 public class Opdracht11_6 extends Applet {
-    
+
     public void init() {
     }
     public void paint(Graphics g) {
+        this.setSize(new Dimension(650,650));
         int teller;
-        int y = 80;
-        int x = 80;
+        int y = 300;
+        int x = 300;
         int w = 20;
         int h = 20;
-        for ( teller = 0; teller <=4; teller++){
-            y -= 10;
-            x -= 10;
-            w += 20;
-            h += 20;
+        for ( teller = 0; teller <=50; teller++){
+            y -= 4;
+            x -= 4;
+            w += 8;
+            h += 8;
             g.drawOval(x,y,w,h);
         }
     }
